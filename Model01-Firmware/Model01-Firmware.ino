8d03c5e610f98eed6bd6fe25dc8784bf4ac15566
Mattias Jansson <fimblo@yanson.org>
2021-09-01 10:55:02 +0200

    moved some keys around
    Both alt keys signal LeftAlt now.
    Language swap (F24) now moved to function-butterfly

--- Model01-Firmware/Model01-Firmware.ino
+++ Model01-Firmware/Model01-Firmware.ino
@@ -47,10 +47,10 @@ KEYMAPS(
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_LeftGui,
-   Key_LeftControl, Key_Backspace, Key_RightAlt, Key_LeftShift,
+   Key_LeftControl, Key_Backspace, Key_LeftAlt, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   Key_F24,  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(GAMING),
+   Key_RightAlt,  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(GAMING),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    M(MACRO_I3_COMMAND),  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
@@ -64,7 +64,7 @@ KEYMAPS(
    ___, Key_Q, Key_W, Key_E, ___, Key_T, Key_Tab,
    ___, Key_A, Key_S, Key_D, Key_F, ___,
    Key_LeftShift, ___, Key_X, Key_C, ___, ___, Key_LeftGui,
-   Key_Spacebar, ___, Key_RightAlt, Key_LeftShift,
+   Key_Spacebar, ___, Key_LeftAlt, Key_LeftShift,
    ShiftToLayer(FUNCTION),
    
    ___, Key_6, Key_7, Key_8, Key_9,  Key_0,  LockLayer(GAMING),
@@ -80,16 +80,19 @@ KEYMAPS(
    Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
    Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
    Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
-   ___, Key_Delete, ___, ___,
+   ___, Key_Delete, ___ , ___,
    ___,
 
    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
    Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
                                Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
-   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
+   Key_F24,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
    ___, ___, Key_Enter, ___,
    ___)
-) // KEYMAPS(
+)
+
+//  Key_PcApplication <- was on butterfly key 
+// KEYMAPS(
 
 /* Re-enable astyle's indent enforcement */
 // *INDENT-ON*
